fe0e8ac1728b92b70f481010a9953257aa4ee6e5
EJHacks <ee1912136@gmail.com>
2025-12-29 10:48:28 -0500

    Refactor project structure: move server.js to root and update package.json for simplified commands

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 // Serve static files from public directory
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve root route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/pages/index.html'));
+  res.sendFile(path.join(__dirname, 'public/pages/index.html'));
 });
 
 // Game rooms storage
